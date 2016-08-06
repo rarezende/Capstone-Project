30ca6a57ce7336f4ff757ff037ff10805ae5a5c6
Rodrigo Rezende <rarezende1@gmail.com>
2016-08-06 19:08:41 -0400

    Changes to Shiny application

--- Main.R
+++ Main.R
@@ -6,7 +6,7 @@ source("Functions.R")
 
 pt = proc.time()
 
-nLines = 50000
+nLines = 200000
 
 con = file("./en_US/en_US.twitter.txt", "r") 
 sampleTwitter = readLines(con, nLines, encoding = "UTF-8")
